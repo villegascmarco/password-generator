b5d182e7c0fc042b0ff159988399ce8f5ff76e81
Villegas Marco <villegascmarco@gmail.com>
2022-07-13 19:27:03 -0500

    feat: modify urls.py

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin #Not use for this project
 from django.urls import path
+from generator import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls), #Not use for this project
+    path('',views.home),
 ]
