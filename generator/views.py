5f8acbaf5607f602801d0be656d6210614aff328
Villegas Marco <villegascmarco@gmail.com>
2022-07-13 19:26:30 -0500

    feat: add views.py file

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+import random
+import string
 
 # Create your views here.
-def about(request):
-    return render(request, 'about.html')
+def home(request):
+    length = int(request.POST.get('length',0))
+    if not length:
+        return render(request,"home.html")
+        
+    options=''
+
+    if request.POST.get('uppercase',''):
+        options+= string.ascii_uppercase
+    if request.POST.get('lowercase',''):
+        options+= string.ascii_lowercase
+    if request.POST.get('numbers',''):
+        options+= string.digits
+    if request.POST.get('symbols',''):
+        options+= string.punctuation
+  
+    generated_password=''
+    for i in range(length):
+        generated_password+= random.choice(options)
+
+    return render(request,"home.html",{'generated_password':generated_password, 'length':length})
